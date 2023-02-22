b8800252f9ba918d57b4b57d5f2f1d4ddabcbaea
Vincent Lanier <vlanier@uoregon.edu>
2023-02-22 15:06:12 -0800

    added simple test script

---
+++ homework05/testscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "TEST ONE"
+
+./ai test1/input.txt test2/message.txt test1_out.txt
+
+diff test1_out.txt test1/result.txt
+
+
+echo "TEST TWO"
+
+./ai test2/input.txt test2/message.txt test2_out.txt
+
+diff test2_out.txt test2/result.txt
+
+
+echo "REALITY CHECK"
+
+diff test2_out.txt test1/result.txt
