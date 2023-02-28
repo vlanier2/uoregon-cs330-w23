681a59b088c46f8a61a4f8d42122d9714b913289
Vincent Lanier <vlanier@uoregon.edu>
2023-02-28 15:47:22 -0800

    implemented rot13 cipher

--- homework06/rcipher.h
+++ homework06/rcipher.h
@@ -7,6 +7,11 @@ using namespace std;
 /* A class that implements a
    ROT13 cipher class. It 
    inherts class CCipher */
-// TODO: Implement this class
+
+class RCipher : public CCipher {
+public:
+   RCipher();
+};
+
 #endif
 
