d92d270a13f5c3b2f8bce94d1847f075d0692ce8
Vincent Lanier <vlanier@uoregon.edu>
2023-02-28 11:07:58 -0800

    added tests for caesar

--- homework06/testscript.sh
+++ homework06/testscript.sh
@@ -14,6 +14,22 @@ rm testfile
 echo "TEST 3 subs ERROR"
 ./cipher -ms -i test3_error/subs/substitution.txt -o testfile 2> errfile
 diff test3_error/subs/cout_error errfile
+rm errfile
+
+echo "TEST 1 caesar"
+./cipher -mc -i test1/caesar/caesar.txt -o testfile
+diff test1/caesar/caesar.txt testfile
+rm testfile
+
+echo "TEST 2 caesar"
+./cipher -mc -i test2/caesar/caesar.txt -o testfile
+diff test2/caesar/caesar.txt testfile
+rm testfile
+
+echo "TEST 3 caesar ERROR"
+./cipher -mc -i test3_error/caesar/caesar.txt -o testfile 2> errfile
+diff test3_error/caesar/cout_error errfile
+rm errfile
 
 echo "REALITY CHECK"
 diff test1/subs/substitution_out.txt test1/subs/substitution.txt
